3128c080045889b59c1d48f69a16ed42a12a6148
popovichppravi <popovichppravi@gmail.com>
2018-02-23 18:51:58 +0300

    9 стр.

---
+++ living.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Официальный сайт УК «СОЛНЕЧНЫЙ»</title>
+
+    <!-- Fonts CSS -->
+    <link rel="stylesheet" href="css/fonts.css">
+
+    <!--Font Awesome-->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+      <!-- flexslider -->
+    <link rel="stylesheet" href="flexslider/css/flexslider.css" type="text/css" media="screen" />
+
+    <!-- fancybox -->
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css" type="text/css" media="screen" />
+
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- Media CSS -->
+    <link rel="stylesheet" href="css/media.css">
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+<!--УДАЛИТЬ-->
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<!--//УДАЛИТЬ-->
+
+
+  </head>
+  <body>
+    <!--CALL BTN-->
+    <a href="#" class="modal-open-btn" data-toggle="modal" data-target="#myModal"><img src="images/open_btn.png"></a>
+    <!--//CALL BTN-->
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-sm-3 col-md-3 col-lg-2 collapse navbar-collapse sidebar" id="navsidebar">
+        <!--SIDEBAR_MENU-->
+          <p class="menu_name">МЕНЮ</p>
+          <ul class="flex-column sidebar_menu">
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">Особенности проекта</a></li>
+            <li><a href="#">Расположение</a></li>
+            <li><a class="active" href="#">Жилые кварталы</a></li>
+            <li><a href="#">Коммерческие площади</a></li>
+            <li><a href="#">Ход строительства</a></li>
+            <li><a href="#">«Солнечный» сегодня</a></li>
+          </ul>
+        <!--SIDEBAR_MENU-->
+          <div class="s_adress">
+                <p>Отдел продаж:</p>
+                <p>г. Екатеринбург</p>
+                <p>ул. Хохрякова, 48</p>
+                <p>тел.: +7 (343) 226-08-42</p>
+                <p>e-mail: sales@forum-gd.ru</p>
+          </div>
+          <img class="s_logo" src="images/sidebar_logo.svg">
+          <div class="s_social">
+            <ul>
+                <li><a href="#"><i class="fab fa-vk"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>              
+          </div>
+          <div class="s_links">
+            <p><a href="#">Проектная декларация</a></p>
+            <p><a href="#">ООО «Форум Солнечные кварталы»</a></p>
+          </div>
+          <div class="site_create">
+            <span>Создание сайта</span>
+            <a href="http://www.tengo.ag"><img src="images/tengo_logo.png"></a>
+          </div>
+        </nav>
+
+        <main class="col-sm-9 offset-sm-3 col-md-9 col-lg-10 offset-md-3 offset-lg-2 right_content">
+          <div class="row" style="margin: 0;">
+            <div class="col-md-9 flex-md-last" style="padding: 0;">
+              <!--TOP NAVMENU-->
+                <nav class="top-navbar navbar navbar-toggleable-md navbar-inverse">
+                  <button class="navbar-toggler navbar-toggler-left hidden-sm-up" type="button" data-toggle="collapse" data-target="#navsidebar" aria-controls="navsidebar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button> 
+                  <a class="top_logo" href="#"><img src="images/top_logo.svg" /></a>
+                    <ul class="top_title"><li><h1>Жилые кварталы</h1></li></ul>
+                </nav>
+              <!--//TOP NAVMENU-->
+              <img src="images/living_bg.jpg" alt="" class="living_bg_img">
+              <div class="living_icons_inner">
+                <ul>
+                  <li><a href="#"><img src="images/living/l4.png"></a></li>
+                  <li><a href="#"><img src="images/living/l5.png"></a></li>
+                  <li><a href="#"><img src="images/living/l3.png"></a></li>
+                  <li><a href="#"><img src="images/living/l1.png"></a></li>
+                  <li><a href="#"><img src="images/living/l4.png"></a></li>
+                  <li><a href="#"><img src="images/living/l3.png"></a></li>
+                  <li><a href="#"><img src="images/living/l1.png"></a></li>
+                  <li><a href="#"><img src="images/living/l2.png"></a></li>
+                  <li><a href="#"><img src="images/living/l2.png"></a></li>
+                </ul>                
+              </div>                
+            </div>
+            <div class="col-md-3 col-md-pull-9" style="padding: 0;" >
+              <div class="sidebar_living">
+                <ul>
+                  <li>
+                    <div class="l_title l1"><div class="l_img"><img src="images/living/l1.png"></div><span>ЗАО «Форум-групп»</span></div>
+                    <p>Отдел продаж: ул. Хохрякова, 48,<br />тел.: +7 (343) 226-08-42<br />e-mail: sales@forum-gd.ru</p>
+                    <p><a href="#">Посмотреть объекты в продаже</a></p>
+                  </li>
+                  <li>
+                    <div class="l_title l2"><div class="l_img"><img src="images/living/l2.png"></div><span>ООО «Брусника. Екатеринбург»</span></div>
+                    <p>Отдел продаж: ул. Малышева, 47А<br />тел.: +7 (343) 222-34-34</p>
+                    <p><a href="#">Посмотреть объекты в продаже</a></p>
+                  </li>
+                  <li>
+                    <div class="l_title l3"><div class="l_img"><img src="images/living/l3.png"></div><span>ГК «Виктория»</span></div>
+                    <p>Отдел продаж: ул. Аптекарская, 48<br />тел.: +7 (343) 345-69-59 / 253-25-75<br />e-mail: sales@visural.ru</p>
+                    <p><a href="#">Посмотреть объекты в продаже</a></p>
+                  </li>
+                  <li>
+                    <div class="l_title l4"><div class="l_img"><img src="images/living/l4.png"></div><span>ООО «Синара-Девелопмент»</span></div>
+                    <p>Отдел продаж: ул. Счастливая, 6<br />тел.: +7 (343) 361-01-28<br />ул. Розы Люксембург, 51<br />тел.: +7 (343) 229-34-05<br />e-mail: sd@sinara-group.com</p>
+                    <p><a href="#">Посмотреть объекты в продаже</a></p>
+                  </li>
+                  <li>
+                    <div class="l_title l5"><div class="l_img"><img src="images/living/l5.png"></div><span>ООО «Астра-Девелопмент»</span></div>
+                    <p>Отдел продаж: ул. Заводская, 45Д<br />тел.: +7 (343) 235-04-20<br />e-mail: info@astra-sk.ru</p>
+                    <p><a href="#">Посмотреть объекты в продаже</a></p>
+                  </li>
+                </ul>                
+              </div>
+            </div>
+
+          </div>
+        </main>
+      </div>
+    </div>
+
+     <script src="js/popper.min.js"></script>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!-- FlexSlider -->
+    <script defer src="flexslider/js/jquery.flexslider.js"></script>
+
+    <!-- fancybox -->
+    <script defer src="js/jquery.fancybox.min.js"></script>
+
+    <!--CUSTOM-->
+    <script src="js/custom.js"></script>
+
+    <a href="#" class="profitbase-widget flipOutY _pfb-wgt"><span>квартиры <br> в наличии</span></a>
+
+    <!-- Modal 1 -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <form>
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Заказать звонок</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Менеджер свяжется с Вами и проконсультирует<br />по всем интересующим вопросам</p>
+          
+    <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Имя</label>
+      <div class="col-sm-10">
+        <input type="name" class="form-control" id="inputEmail3">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Телефон</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputPassword3">
+      </div>
+    </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-warning">Отправить</button>
+      </div>
+    </div>
+     </form>
+  </div>
+</div>
+  </body>
+</html>
+
+
---
+++ sun-in.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Официальный сайт УК «СОЛНЕЧНЫЙ»</title>
+
+    <!-- Fonts CSS -->
+    <link rel="stylesheet" href="css/fonts.css">
+
+    <!--Font Awesome-->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+      <!-- flexslider -->
+    <link rel="stylesheet" href="flexslider/css/flexslider.css" type="text/css" media="screen" />
+
+    <!-- fancybox -->
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css" type="text/css" media="screen" />
+
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="styles.css">
+
+    <!-- Media CSS -->
+    <link rel="stylesheet" href="css/media.css">
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+
+
+<!--Удалить-->
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+<!--//удалить-->
+
+  </head>
+  <body>
+    <!--CALL BTN-->
+    <a href="#" class="modal-open-btn" data-toggle="modal" data-target="#myModal"><img src="images/open_btn.png"></a>
+    <!--//CALL BTN-->
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-sm-3 col-md-3 col-lg-2 collapse navbar-collapse sidebar" id="navsidebar">
+        <!--SIDEBAR_MENU-->
+          <p class="menu_name">МЕНЮ</p>
+          <ul class="flex-column sidebar_menu">
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">Особенности проекта</a></li>
+            <li><a href="#">Расположение</a></li>
+            <li><a href="#">Жилые кварталы</a></li>
+            <li><a href="#">Коммерческие площади</a></li>
+            <li><a href="#">Ход строительства</a></li>
+            <li><a href="#" class="active">«Солнечный» сегодня</a></li>
+          </ul>
+        <!--SIDEBAR_MENU-->
+          <div class="s_adress">
+                <p>Отдел продаж:</p>
+                <p>г. Екатеринбург</p>
+                <p>ул. Хохрякова, 48</p>
+                <p>тел.: +7 (343) 226-08-42</p>
+                <p>e-mail: sales@forum-gd.ru</p>
+          </div>
+          <img class="s_logo" src="images/sidebar_logo.svg">
+          <div class="s_social">
+            <ul>
+                <li><a href="#"><i class="fab fa-vk"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>              
+          </div>
+          <div class="s_links">
+            <p><a href="#">Проектная декларация</a></p>
+            <p><a href="#">ООО «Форум Солнечные кварталы»</a></p>
+          </div>
+          <div class="site_create">
+            <span>Создание сайта</span>
+            <a href="http://www.tengo.ag"><img src="images/tengo_logo.png"></a>
+          </div>
+        </nav>
+
+        <main class="col-sm-9 offset-sm-3 col-md-9 col-lg-10 offset-md-3 offset-lg-2 right_content">
+
+          <!--TOP NAVMENU-->
+            <nav class="top-navbar navbar navbar-toggleable-md navbar-inverse">
+                <button class="navbar-toggler navbar-toggler-left hidden-sm-up" type="button" data-toggle="collapse" data-target="#navsidebar" aria-controls="navsidebar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+             
+              <a class="top_logo" href="#"><img src="images/top_logo.svg" /></a>
+                <ul class="top_title"><li><h1>Зима в «Солнечном» 2017-2018</h1></li></ul>
+
+            </nav>
+          <!--//TOP NAVMENU-->
+          
+          <div class="photo_groups">
+             <div class="photo_items after_sb">
+                <div class="row">
+                   <div class="col-md-4 ph_item">
+                     <a href="images/pj1.jpg" data-fancybox="gallery">
+                      <img src="images/pj1.jpg">
+                      <p><span>Свой парк и современное благоустройство</span></p>
+                     </a>
+                   </div>
+                   <div class="col-md-4 ph_item">
+                      <a href="images/pj2.jpg" data-fancybox="gallery">
+                        <img src="images/pj2.jpg">
+                        <p><span>Активно развивающаяся социальная инфраструктура</span></p>
+                      </a>
+                    </div>
+                   <div class="col-md-4 ph_item">
+                    <a href="images/pj3.jpg" data-fancybox="gallery">
+                      <img src="images/pj3.jpg">
+                      <p><span>Уютная атмосфера и дружелюбные соседи</span></p>
+                    </a>
+                   </div> 
+                </div>
+                <div class="row">
+                   <div class="col-md-4 ph_item">
+                     <a href="images/pj4.jpg" data-fancybox="gallery">
+                      <img src="images/pj4.jpg">
+                      <p><span>Комфортная этажность и плотность застройки</span></p>
+                     </a>
+                   </div>
+                   <div class="col-md-4 ph_item">
+                      <a href="images/pj5.jpg" data-fancybox="gallery">
+                        <img src="images/pj5.jpg">
+                        <p><span>Разнообразие архитектурных и планировочных решений</span></p>
+                      </a>
+                    </div>
+                   <div class="col-md-4 ph_item">
+                    <a href="images/pj6.jpg" data-fancybox="gallery">
+                      <img src="images/pj6.jpg">
+                      <p><span>Удобные парковки</span></p>
+                    </a>
+                   </div> 
+                </div>
+                <div class="row">
+                   <div class="col-md-4 ph_item">
+                     <a href="images/pj7.jpg" data-fancybox="gallery">
+                      <img src="images/pj7.jpg">
+                      <p><span>Комплексная система безопасности</span></p>
+                     </a>
+                   </div>
+                   <div class="col-md-4 ph_item">
+                      <a href="images/pj8.jpg" data-fancybox="gallery">
+                        <img src="images/pj8.jpg">
+                        <p><span>Единая Управляющая компания</span></p>
+                      </a>
+                    </div>
+                   <div class="col-md-4 ph_item">
+                    <a href="images/pj9.jpg" data-fancybox="gallery">
+                      <img src="images/pj9.jpg">
+                      <p><span>Современная система инженерного обеспечения</span></p>
+                    </a>
+                   </div> 
+                </div>                             
+            </div>        
+          
+          </div>
+        </main>
+      </div>
+    </div>
+
+     <script src="js/popper.min.js"></script>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+    <!-- FlexSlider -->
+    <script defer src="flexslider/js/jquery.flexslider.js"></script>
+
+    <!-- fancybox -->
+    <script defer src="js/jquery.fancybox.min.js"></script>
+
+    <!--CUSTOM-->
+    <script src="js/custom.js"></script>
+
+    <a href="#" class="profitbase-widget flipOutY _pfb-wgt"><span>квартиры <br> в наличии</span></a>
+
+    <!-- Modal 1 -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <form>
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Заказать звонок</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Менеджер свяжется с Вами и проконсультирует<br />по всем интересующим вопросам</p>
+          
+    <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Имя</label>
+      <div class="col-sm-10">
+        <input type="name" class="form-control" id="inputEmail3">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Телефон</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="inputPassword3">
+      </div>
+    </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-warning">Отправить</button>
+      </div>
+    </div>
+     </form>
+  </div>
+</div>
+  </body>
+</html>
+
+
